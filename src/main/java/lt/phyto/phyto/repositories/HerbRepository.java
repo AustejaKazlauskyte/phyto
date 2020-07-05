9376d98b64e6671be5e7ff591341782f62d84022
Austeja Kazlauskyte <akazlauskyte@gmail.com>
2020-07-05 11:38:14 +0300

    Added repository annotations, wont compile

--- src/main/java/lt/phyto/phyto/repositories/HerbRepository.java
+++ src/main/java/lt/phyto/phyto/repositories/HerbRepository.java
@@ -4,7 +4,6 @@ import lt.phyto.phyto.models.entities.HerbEntity;
 import org.springframework.data.repository.Repository;
 import java.util.Set;
 
-@org.springframework.stereotype.Repository
 public interface HerbRepository extends Repository<HerbEntity, Long> {
 
   HerbEntity findOneById(long id);
