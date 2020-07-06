02983cf3f29d903011b49aa02b4177c3f39f658e
Austeja Kazlauskyte <akazlauskyte@gmail.com>
2020-07-06 16:00:21 +0300

    Changed validate to update, it compiled

--- src/main/java/lt/phyto/phyto/repositories/CharacteristicRepository.java
+++ src/main/java/lt/phyto/phyto/repositories/CharacteristicRepository.java
@@ -1,7 +1,6 @@
 package lt.phyto.phyto.repositories;
 
 import lt.phyto.phyto.models.entities.CharacteristicEntity;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.Repository;
 
 
