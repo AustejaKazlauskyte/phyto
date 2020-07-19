2851564abe736a6e57f5fdc66a12a89cf2f5d12c
Austeja Kazlauskyte <akazlauskyte@gmail.com>
2020-07-19 11:18:41 +0300

    Added Post and Delete features

--- src/main/java/lt/phyto/phyto/models/entities/HerbEntity.java
+++ src/main/java/lt/phyto/phyto/models/entities/HerbEntity.java
@@ -17,14 +17,14 @@ public class HerbEntity extends AbstractEntity {
   @Column(name = "name", length = 250, nullable = false)
   private String name;
 
-  @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+  @ManyToMany(cascade = CascadeType.ALL)
   @JoinTable(
       name = "herb_characteristic",
       joinColumns = @JoinColumn(name = "herb_id"),
       inverseJoinColumns = @JoinColumn(name = "characteristic_id"))
   private Set<CharacteristicEntity> characteristics = new HashSet<>();
 
-  @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+  @ManyToMany(cascade = CascadeType.ALL)
   @JoinTable(
       name = "herb_medical_condition",
       joinColumns = @JoinColumn(name = "herb_id"),
